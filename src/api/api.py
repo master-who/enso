9008819c14d21b706aca6187c657aef5e6fa0c27
Who <who@emptycup.in>
2025-07-14 21:03:48 +0530

    Added /subscribe

--- src/api/api.py
+++ src/api/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, send_from_directory
 from flask import render_template
 import os
+from flask import request
 
 api = Flask(__name__)
 
@@ -15,6 +16,14 @@ def manifest():
 @api.route('/sw.js')
 def service_worker():
     return send_from_directory('static', 'sw.js')
+
+@api.route('/subscribe', methods=['POST'])
+def subscribe():
+    data = request.get_json()
+    save_path = os.path.join('subscription.json')
+    with open(save_path, 'w', encoding='utf-8') as f:
+        f.write(f"{data}\n")
+    return jsonify({'status': 'success'}), 201
 
 if __name__ == '__main__':
     api.run(debug=True)
